8e16d66364a564052f502ec1a26c4c5476257e77
Remus670 <remus.catalin12@gmail.com>
2026-04-18 19:10:35 +0200

    chore: remove old UI scaffolds, DeviceManagementUI is the active project

--- frontend/src/app/components/device-detail/device-detail.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-device-detail',
-  imports: [],
-  templateUrl: './device-detail.html',
-  styleUrl: './device-detail.css',
-})
-export class DeviceDetail {}
